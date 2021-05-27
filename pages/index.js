1550724ae97e8b4a7f9fbed4b4465d355ce1dabc
PezeM <16854655+PezeM@users.noreply.github.com>
2021-05-27 23:26:32 +0200

    Added chakra global theme

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,39 @@
-import Head from 'next/head'
-import { useAuth } from '../lib/auth'
+import Head from 'next/head';
+import {useAuth} from '../lib/auth'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const auth = useAuth();
+    const auth = useAuth();
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Comments Provider</title>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Comments Provider Demo
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Comments Provider Demo
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <p className={styles.description}>
+                    Get started by editing{' '}
+                    <code className={styles.code}>pages/index.js</code>
+                </p>
 
-      <div>
-        <button onClick={() => auth.signIn()}>Sign In</button>
+                <div>
+                    <button onClick={() => auth.signIn()}>Sign In</button>
 
-        <div>
-          {auth?.user?.email}
+                    <div>
+                        {auth?.user?.email}
+                    </div>
+
+                    {auth?.user && (
+                        <button onClick={() => auth.signOut()}>Sign Out</button>
+                    )}
+                </div>
+
+            </main>
         </div>
-
-        {auth?.user && (
-          <button onClick={() => auth.signOut()}>Sign Out</button>
-        )}
-      </div>
-
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
--- styles/theme.js
+++ styles/theme.js
@@ -8,14 +8,18 @@ const colors = {
     },
 }
 
+const fontWeights = {
+    normal: 400,
+    medium: 600,
+    bold: 700
+}
+
+const fonts = {
+    body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`
+}
+
 export const theme = extendTheme({
     colors,
-    fonts: {
-
-    },
-    fontWeights: {
-        normal: 400,
-        medium: 600,
-        bold: 700
-    }
+    fontWeights,
+    fonts
 });
