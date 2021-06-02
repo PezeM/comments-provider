4b85cbda8b1dd448e95cb907c9044e7d6a31267b
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-02 19:48:45 +0200

    Updated feedback link with new paths

--- components/Feedback/FeedbackLink.js
+++ components/Feedback/FeedbackLink.js
@@ -1,12 +1,19 @@
 import { Flex, Link } from '@chakra-ui/react';
 
-export const FeedbackLink = ({ siteId }) => {
+export const FeedbackLink = ({ paths }) => {
   return (
-    <Flex justifyContent="space-between" mb={8} width="full" mt={1}>
-      <Link fontWeight="bold" fontSize="sm" href={`/sites/${siteId}`}>
+    <Flex
+      align={['flex-start', 'center']}
+      justifyContent="space-between"
+      mb={8}
+      width="full"
+      mt={1}
+      direction={['column', 'row']}
+    >
+      <Link fontWeight="bold" fontSize="sm" href={`/sites/${paths.join('/')}`} target={'_blank'}>
         Leave a comment →
       </Link>
-      <Link fontSize="xs" color="blackAlpha.500" href="/">
+      <Link fontSize="xs" color="blackAlpha.500" href="/" target={'_blank'}>
         Powered by Comments Provider
       </Link>
     </Flex>
