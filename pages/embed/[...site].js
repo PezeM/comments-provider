4509558dc26c450069544c4bf5b4b12ed4fed11c
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-02 22:07:10 +0200

    Added embed comments component

---
+++ pages/embed/[...site].js
@@ -0,0 +1,55 @@
+import { useRouter } from 'next/router';
+import { getAllFeedback, getAllSites, getSite } from '@/lib/database-admin';
+import { Box } from '@chakra-ui/react';
+import { Feedback } from '@/components/Feedback/Feedback';
+import { FeedbackLink } from '@/components/Feedback/FeedbackLink';
+
+export async function getStaticProps(context) {
+  const [siteId, route] = context.params.site;
+  const { feedbacks } = await getAllFeedback(siteId, route);
+  const { site } = await getSite(siteId);
+
+  return {
+    props: {
+      initialFeedback: feedbacks,
+      site,
+    },
+    revalidate: true,
+  };
+}
+
+export async function getStaticPaths() {
+  const { sites } = await getAllSites();
+  const paths = sites.map(site => ({
+    params: {
+      site: [site.id.toString()],
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export default function EmbeddedFeedbackPage({ initialFeedback, site }) {
+  const router = useRouter();
+
+  return (
+    <Box display="flex" flexDirection="column" width="full">
+      <FeedbackLink paths={router.query.site} />
+      {initialFeedback?.length ? (
+        initialFeedback.map((feedback, index) => (
+          <Feedback
+            key={feedback.id}
+            settings={site?.settings}
+            isLast={index === initialFeedback.length - 1}
+            {...feedback}
+          />
+        ))
+      ) : (
+        <Box>There are no comments for this site.</Box>
+      )}
+    </Box>
+  );
+}
