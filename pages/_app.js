8131c7921ee19413154d25950f1d7fc2561e823e
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-03 21:00:15 +0200

    Set viewport in app

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,15 @@ import { theme } from '@/styles/theme';
 import '@/styles/global.css';
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
+import { Head } from 'next/document';
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
         <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </AuthProvider>
