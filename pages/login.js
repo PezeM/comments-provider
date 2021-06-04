9eacb864cbf4679f420e313979d791dad0dcc087
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-04 12:19:50 +0200

    Refactored login page

--- pages/login.js
+++ pages/login.js
@@ -1,53 +1,26 @@
 import { Page } from '@/components/Page';
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  Stack,
-  useToast,
-} from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
+import { Box, Button, Flex, Stack, Text } from '@chakra-ui/react';
 import { useAuth } from '@/lib/auth';
-import React, { useState } from 'react';
-import { LogoIcon } from '@/styles/icons';
+import React, { useEffect } from 'react';
+import { GithubIcon, GoogleIcon, LogoIcon } from '@/styles/icons';
+import { useRouter } from 'next/router';
 
 const Login = () => {
-  const toast = useToast();
-  const [loading, setLoading] = useState(false);
-  const { signinWithEmail } = useAuth();
-  const { handleSubmit, register, errors } = useForm();
+  const auth = useAuth();
+  const router = useRouter();
 
-  const onLogin = async ({ email, pass }) => {
-    setLoading(true);
-
-    try {
-      await signinWithEmail(email, pass);
-    } catch (e) {
-      setLoading(false);
-
-      toast({
-        title: 'An error occurred.',
-        description: e.message,
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-      });
+  useEffect(() => {
+    if (auth.user) {
+      router.push('/sites');
     }
-  };
+  }, []);
 
   return (
-    <Flex align="center" justify="center" h="100vh" backgroundColor="gray.100">
+    <Flex align="center" justify="center" h="100vh" backgroundColor={['white', 'gray.100']}>
       <Stack
-        as="form"
         backgroundColor="white"
         borderRadius={[0, 8]}
-        errors={errors}
         maxWidth="400px"
-        onSubmit={handleSubmit(data => onLogin(data))}
         px={8}
         py={12}
         shadow={[null, 'md']}
@@ -55,57 +28,52 @@ const Login = () => {
         w="100%"
       >
         <Flex justify="center">
-          <Box as="a" href="/" aria-label="Back to homepage">
-            <LogoIcon color="black" boxSize={8} />
+          <Box
+            as="a"
+            href="/"
+            aria-label="Back to homepage"
+            mb={6}
+            alignItems={'center'}
+            flexDirection={'row'}
+            display={'flex'}
+          >
+            <LogoIcon color="black" boxSize={8} mr={4} />
+            <Text>Back to homepage</Text>
           </Box>
         </Flex>
-
-        <FormControl isInvalid={errors?.email && errors?.email?.message}>
-          <FormLabel>Email Address</FormLabel>
-          <Input
-            autoFocus
-            aria-label="Email Address"
-            id="email"
-            name="email"
-            placeholder="example@example.com"
-            {...register('email', {
-              required: 'Enter your email.',
-            })}
-          />
-          <FormErrorMessage>{errors?.email && errors.email?.message}</FormErrorMessage>
-        </FormControl>
-
-        <FormControl isInvalid={errors?.pass && errors.pass?.message}>
-          <FormLabel>Password</FormLabel>
-          <Input
-            aria-label="Password"
-            name="pass"
-            id="password"
-            type="password"
-            {...register('pass', {
-              required: 'Enter your password.',
-            })}
-          />
-          <FormErrorMessage>{errors?.pass && errors.pass?.message}</FormErrorMessage>
-        </FormControl>
-
         <Button
-          id="login"
-          type="submit"
+          onClick={() => auth.signInWithGithub('/sites')}
           backgroundColor="gray.900"
           color="white"
-          isLoading={loading}
           fontWeight="medium"
-          mt={4}
+          leftIcon={<GithubIcon />}
+          mt={2}
           h="50px"
-          fontSize="lg"
           _hover={{ bg: 'gray.700' }}
           _active={{
             bg: 'gray.800',
             transform: 'scale(0.95)',
           }}
         >
-          Login
+          Continue with GitHub
+        </Button>
+
+        <Button
+          onClick={() => auth.signInWithGoogle('/sites')}
+          backgroundColor="white"
+          color="gray.900"
+          variant="outline"
+          fontWeight="medium"
+          leftIcon={<GoogleIcon />}
+          mt={2}
+          h="50px"
+          _hover={{ bg: 'gray.100' }}
+          _active={{
+            bg: 'gray.100',
+            transform: 'scale(0.95)',
+          }}
+        >
+          Continue with Google
         </Button>
       </Stack>
     </Flex>
