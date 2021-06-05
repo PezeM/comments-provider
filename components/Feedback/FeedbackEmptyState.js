424b4971678d9713d4901dcd87ebce0f0967b262
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-05 22:50:50 +0200

    Added dark mode to feedback empty state

--- components/Feedback/FeedbackEmptyState.js
+++ components/Feedback/FeedbackEmptyState.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Flex, Heading, Text } from '@chakra-ui/react';
+import {useBoxBackgroundColor} from "@/styles/hooks/useBoxBackgroundColor";
 
-export const FeedbackEmptyState = () => (
-  <Flex
-    width="100%"
-    backgroundColor="white"
-    borderRadius="8px"
-    p={16}
-    justify="center"
-    align="center"
-    direction="column"
-  >
-    <Heading size="lg" mb={2}>
-      There is not feedback added.
-    </Heading>
-    <Text mb={4}>Add new comments</Text>
-  </Flex>
-);
+export const FeedbackEmptyState = () => {
+  const boxBg = useBoxBackgroundColor();
+
+  return (
+    <Flex
+      width="100%"
+      backgroundColor={boxBg}
+      borderRadius="8px"
+      p={16}
+      justify="center"
+      align="center"
+      direction="column"
+    >
+      <Heading size="lg" mb={2}>
+        There is not feedback added.
+      </Heading>
+      <Text mb={4}>Add new comments</Text>
+    </Flex>
+  );
+};
