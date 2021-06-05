40e7e95adab7e975920aab0ea6c67d7bb9b5e4cf
PezeM <16854655+PezeM@users.noreply.github.com>
2021-06-05 22:48:58 +0200

    Fixed feedback usage dark theme

--- components/Account/FeedbackUsage.js
+++ components/Account/FeedbackUsage.js
@@ -1,16 +1,25 @@
-import { Stat, StatGroup, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
+import {
+  Stat,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 export const FeedbackUsage = () => {
+  const color = useColorModeValue('gray.700', 'gray.100');
+
   return (
     <StatGroup>
       <Stat>
-        <StatLabel color="gray.700">Comments</StatLabel>
+        <StatLabel color={color}>Comments</StatLabel>
         <StatNumber fontWeight="medium">∞</StatNumber>
         <StatHelpText>10,000 limit</StatHelpText>
       </Stat>
 
       <Stat>
-        <StatLabel color="gray.700">Sites</StatLabel>
+        <StatLabel color={color}>Sites</StatLabel>
         <StatNumber fontWeight="medium">1/∞</StatNumber>
         <StatHelpText>Unlimited Sites</StatHelpText>
       </Stat>
